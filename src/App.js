a96e4a2b2f90e1138da9d775fde9be216c206401
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2022-11-15 16:25:00 +0530

    commmit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-// import logo from './logo.svg';
-import { useState } from 'react';
 import './App.css';
-import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-
-
-
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-  const [mode, setDarkMode] =  useState('light');
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
 
-  const toggleMode = ()=> {
+  const toggleMode = ()=>{
     if(mode === 'light'){
-      setDarkMode('dark');
-      document.body.style.backgroundColor = 'grey';
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+     
     }
     else{
-      setDarkMode('light');
+      setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      // document.title = 'TextUtils - Light Mode';
     }
-
   }
   return (
     <>
-      <Navbar title="Text-Utils" mode={mode} toggleMode = {toggleMode}/>
-
-      <div className="container my-3">
-        <TextForm heading="Enter Text To Analyse" mode={mode} />
-        {/* <About /> */}
-      </div>
-    
-    </>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+          <Route exact path="/about">
+            <About  />
+          </Route>
+          <Route exact path="/">
+            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+          </Route>
+    </Switch>
+    </div>
+    </Router>
+    </> 
   );
 }
 
